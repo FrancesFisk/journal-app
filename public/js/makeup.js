5740a238e8391019bfcb9e0fb33a435ea387f711
Glitch (quasar-nut) <none>
2018-06-15 17:09:27 -0700

    background and nav styles

--- public/js/makeup.js
+++ public/js/makeup.js
@@ -314,4 +314,21 @@ $( '.modal-content' ).on('click', '.edit-btn', function(e) {
 });
 
 
+//  ******* NAV MENU ********
+$('.toggle-nav').click(function() {
+  // Calling a function in case you want to expand upon this.
+  toggleNav();
+});  
+
+
+function toggleNav() {
+if ($('#site-wrapper').hasClass('show-nav')) {
+  // Do things on Nav Close
+  $('#site-wrapper').removeClass('show-nav');
+} else {
+  // Do things on Nav Open
+  $('#site-wrapper').addClass('show-nav');
+}
+}
+
 });
